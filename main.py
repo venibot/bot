724615e03bbc4a973fcad5be5fc668c88041d584
Михаил <barinovmishka@mail.ru>
2021-01-04 15:46:02 +0300

    Данные снова отправляются на мониторинги(случайно закомментировал ранее)

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ async def on_ready():
     Database.create_tables()
     for guild in client.guilds:
         Database.add_guild(guild)
-    # SDC.send_stat(len(client.guilds), len(client.shards))
-    # BotiCord.send_stat(len(client.guilds), len(client.shards), len(client.users))
+    SDC.send_stat(len(client.guilds), len(client.shards))
+    BotiCord.send_stat(len(client.guilds), len(client.shards), len(client.users))
 
 
 client.run(TOKEN)
